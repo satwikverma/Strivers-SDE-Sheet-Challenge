33121338e14d647a87ec0a948e918b36a886925c
Satwik Verma <satwikverma18@gmail.com>
2022-07-01 08:04:21 +0530

    D1P1-Brute1

---
+++ 1. Arrays/01. Set_Matrix_Zeroes(Brute).cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    void setZeroes(vector<vector < int>> &matrix)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<pair<int, int>> zeroes;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    pair<int, int> p = make_pair(i, j);
+                    zeroes.push_back(p);
+                }
+            }
+        }
+
+        for (int x = 0; x < zeroes.size(); x++)
+        {
+            int i = zeroes[x].first;
+            int j = zeroes[x].second;
+
+            for (int k = 0; k < n; k++)
+                matrix[i][k] = 0;
+            for (int k = 0; k < m; k++)
+                matrix[k][j] = 0;
+        }
+    }
+};
